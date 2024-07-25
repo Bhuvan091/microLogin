9dfae631605e0427fb2999b21c124391f7e734c8
Bhuvan091 <96046149+Bhuvan091@users.noreply.github.com>
2024-07-25 17:36:18 +0530

    Update App.tsx

--- microsoftLogin/src/App.tsx
+++ microsoftLogin/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
   import { PageLayout } from './components/PageLayout';
   import { loginRequest } from './authConfig';
